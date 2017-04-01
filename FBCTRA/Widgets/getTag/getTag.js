f096e1b357e61b2f0151deba91479b696bc79daf
rtaylorSWC <rtaylor@swcgroup.com>
2017-04-01 13:15:17 -0500

    placeholder for css refactor

--- FBCTRA/Widgets/getTag/getTag.js
+++ FBCTRA/Widgets/getTag/getTag.js
@@ -1,21 +1,9 @@
 ﻿angular.module('GetTagModule', [])
-  .controller('GetTagController', ['$scope', '$state', 'messageBus',
-	function ($scope, $state, messageBus) {
+  .controller('GetTagController', ['$scope', '$window',
+	function ($scope, $window) {
 	    'use strict';
-
-	    $scope.tabs = [{
-	        route: 'recieveNotice',
-	        titleId: 'LABEL_INFO_RECIEVE_NOTICE',
-	        contentId: 'CONTENT_RECIEVE_NOTICE'
-	    }];
-
-	    $scope.isActive = function (viewRoute) {
-	        //return $state.current.name.indexOf(viewRoute) >= 0;
-	        return false;
-	    };
-
-	    $scope.open = function (tab) {
-	        messageBus.publish('itemSelected', tab);
+	    $scope.openUrlInTab = function (url) {
+	        $window.open(url, '_blank');
 	    };
 	}
   ]);
