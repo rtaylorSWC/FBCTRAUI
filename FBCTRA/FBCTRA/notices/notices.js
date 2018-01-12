58c10d9c4ddd60fd5164acb89c4086a232dda7da
Reginald <Reginald@DESKTOP-UKKC6SC>
2018-01-12 14:16:05 -0600

    Fix undefined error

--- FBCTRA/FBCTRA/notices/notices.js
+++ FBCTRA/FBCTRA/notices/notices.js
@@ -1,184 +1,188 @@
 ﻿angular.module('FBCApp')
-  .controller('NoticesController', ['$scope', '$filter', '$translate', '$base64', '$interval', 'localStore', 'messageBus', 'FlashService', 'AccountService', 'PaymentService',
-    function ($scope, $filter, $translate, $base64, $interval, localStore, messageBus, FlashService, AccountService, PaymentService) {
-        'use strict';
+    .controller('NoticesController', ['$rootScope', '$scope', '$filter', '$translate', '$base64', '$interval', 'localStore', 'messageBus', 'FlashService', 'AccountService', 'PaymentService',
+        function ($rootScope, $scope, $filter, $translate, $base64, $interval, localStore, messageBus, FlashService, AccountService, PaymentService) {
+            'use strict';
 
-        messageBus.subscribe($scope, 'payItemClosed', function (event, payItem) {
-            $scope.getPaymentStatus(payItem);
-            $scope.paymentTotal = "0.00";
+            messageBus.subscribe($scope, 'payItemClosed', function (event, payItem) {
+                $scope.getPaymentStatus(payItem);
+                $scope.paymentTotal = "0.00";
+                $scope.idSelected = false;
+                $scope.itemSelected = false;
+                $scope.selection = [];
+                $scope.noticeNumbers = [];
+            });
+
+            var currentUser = localStore.getCurrentUser();
+            currentUser = currentUser.currentUser;
+            $scope.paymentMethod = $scope.paymentTotal = false;
+            var sortingOrder = 'name'; //default sort
+            $scope.sortingOrder = sortingOrder;
+            $scope.reverse = false;
+            $scope.filteredItems = [];
             $scope.idSelected = false;
             $scope.itemSelected = false;
             $scope.selection = [];
             $scope.noticeNumbers = [];
-        });
+            $scope.NSFFeeAdded = false;
+            $scope.showNSFStatus = false;
 
-        var currentUser = localStore.getCurrentUser();
-        currentUser = currentUser.currentUser;
-        $scope.paymentMethod = $scope.paymentTotal = false;
-        var sortingOrder = 'name'; //default sort
-        $scope.sortingOrder = sortingOrder;
-        $scope.reverse = false;
-        $scope.filteredItems = [];
-        $scope.idSelected = false;
-        $scope.itemSelected = false;
-        $scope.selection = [];
-        $scope.noticeNumbers = [];
-        $scope.NSFFeeAdded = false;
-        $scope.showNSFStatus = false;
-
-        $scope.openPaymentModal = function (payItem) {
-            messageBus.publish('iFrameItemSelected', payItem);
-        };
-
-        $scope.openImageModal = function (imageUrl) {
-            var imageData = {};
-            imageData.showImage = true;
-            imageData.titleId = '';
-            imageData.contentId = imageUrl;
-            messageBus.publish('iFrameItemSelected', imageData);
-        };
-
-        $scope.getViolationList = function () {
-            AccountService.getViolationsByAccountGuid(currentUser.AccountGuid, function (response) {
-                if (response) {
-                    $scope.violationData = response;
-                    $scope.showNSFStatus = ($scope.violationData.NSFStatus !== '' && $scope.violationData.NSFStatus !== null) ? true : false;
-                } else {
-                    response.Message ? FlashService.Error(response.Message) : FlashService.Error("Unable to get Violation List.");
-                }
-            });
-        };
-
-        $scope.getNoticeTransactions = function ($index, notice) {
-            if (notice.HasDetails == 'Yes') {
-                $scope.selectedNotice = notice;
-                AccountService.getTransactionsByAccountGuidAndTVNID(currentUser.AccountGuid, notice.TVNID, function (response) {
-                    if (response) {
-                        $scope.violationData.Violations[$index].noticeDetailData = response;
-                    } else {
-                        response.Message ? FlashService.Error(response.Message) : FlashService.Error("Unable to get Transaction Details.");
-                    }
-                });
-            }
-            $scope.idSelected = notice.NoticeNumber;
-        };
-
-        $scope.disputeNotice = function (notice) {
-            localStore.setCurrentNotice(notice);
-            var metaData = [];
-            metaData.titleId = 'LINK_HELP_TITLE_DISPUTE';
-            metaData.templateUrl = 'Widgets/helpdesk/helpdesk.html';
-            messageBus.publish('openHelpDesk', metaData);
-        };
-
-        $scope.getNoticePdf = function (fileName) {
-            var statementData = {
-                "FileName": fileName,
-                "DirectoryName": ""
+            $scope.openPaymentModal = function (payItem) {
+                messageBus.publish('iFrameItemSelected', payItem);
             };
-            var fileName = "ViolationStatement_" + fileName;
-            var a = document.createElement("a");
-            document.body.appendChild(a);
-            AccountService.getNoticePdf(statementData, function (response) {
-                if (response) {
-                    var file = new Blob([response.data], { type: 'application/pdf' });
-                    var fileURL = URL.createObjectURL(file);
-                    a.href = fileURL;
-                    a.download = fileName;
-                    a.click();
-                } else {
-                    response.Message ? FlashService.Error(response.Message) : FlashService.Error("Unable to get notice pdf.");
-                }
-            });
-        };
 
-        $scope.getPaymentUrl = function () {
-            if ($scope.paymentTotal != "0.00" && $scope.paymentMethod) {
-                $scope.paymentData = {};
-                $scope.paymentData.AccountGuId = currentUser.AccountGuid;
-                $scope.paymentData.Amount = parseFloat($scope.paymentTotal);
-                $scope.paymentData.Paymethod = $scope.paymentMethod;
-                $scope.paymentData.Language = ($translate.use() == 'en') ? "English" : "Spanish";
-                $scope.NSFFeeAdded && ($scope.noticeNumbers.indexOf("NSF") == -1) ? $scope.noticeNumbers.push("NSF") : null;
-                $scope.paymentData.NoticeNumbers = $scope.noticeNumbers;
+            $scope.openImageModal = function (imageUrl) {
+                var imageData = {};
+                imageData.showImage = true;
+                imageData.titleId = '';
+                imageData.contentId = imageUrl;
+                messageBus.publish('iFrameItemSelected', imageData);
+            };
 
-                PaymentService.getPaymentURL($scope.paymentData, function (response) {
+            $scope.getViolationList = function () {
+                AccountService.getViolationsByAccountGuid(currentUser.AccountGuid, function (response) {
                     if (response) {
-                        $scope.payment = {};
-                        $scope.payment.titleId = "FBCTRA Payment Portal";
-                        $scope.payment.contentId = response.paymentUrl;
-                        $scope.payment.data = response;
-                        $scope.payment.showIFrame = true;
-                        $scope.openPaymentModal($scope.payment);
+                        $scope.violationData = response;
+                        $scope.showNSFStatus = ($scope.violationData.NSFStatus !== '' && $scope.violationData.NSFStatus !== null) ? true : false;
                     } else {
-                        response.Message ? FlashService.Error(response.Message) : FlashService.Error("Unable to get Payment URL.");
+                        response.Message ? FlashService.Error(response.Message) : FlashService.Error("Unable to get Violation List.");
                     }
                 });
-            }
-        };
+            };
 
-        $scope.getPaymentStatus = function (paymentData) {
-            PaymentService.getPaymentStatus(paymentData, function (response) {
-                if (response) {
-                    $scope.payment.data = response;
-                    $scope.getViolationList();
+            $scope.getNoticeTransactions = function ($index, notice) {
+                if (notice.HasDetails == 'Yes') {
+                    $scope.selectedNotice = notice;
+                    AccountService.getTransactionsByAccountGuidAndTVNID(currentUser.AccountGuid, notice.TVNID, function (response) {
+                        if (response) {
+                            $scope.violationData.Violations[$index].noticeDetailData = response;
+                        } else {
+                            response.Message ? FlashService.Error(response.Message) : FlashService.Error("Unable to get Transaction Details.");
+                        }
+                    });
                 }
-            });
-        };
+                $scope.idSelected = notice.NoticeNumber;
+            };
 
-        $scope.statusPending = function (status) {
-            return status.toUpperCase() === 'PENDING';
-        };
+            $scope.disputeNotice = function (notice) {
+                localStore.setCurrentNotice(notice);
+                var metaData = [];
+                metaData.titleId = 'LINK_HELP_TITLE_DISPUTE';
+                metaData.templateUrl = 'Widgets/helpdesk/helpdesk.html';
+                messageBus.publish('openHelpDesk', metaData);
+            };
 
-        $scope.toggleSelection = function toggleSelection($event, item) {
-            if (item.Payable === '1') {
-                $event.stopPropagation();
-                var idx = $scope.selection.indexOf(item);
-                (idx > -1) ? $scope.selection.splice(idx, 1) : $scope.selection.push(item);
-                var idx2 = $scope.noticeNumbers.indexOf(item.NoticeNumber);
-                (idx2 > -1) ? $scope.noticeNumbers.splice(idx2, 1) : $scope.noticeNumbers.push(item.NoticeNumber);
-            }
-        };
-
-        $scope.$watchCollection('selection', function (array) {
-            if (array) {
-                $scope.paymentTotal = array.reduce(function (total, item) {
-                    return total + parseFloat(item.AmountDue);
-                }, 0).toFixed(2);
-                if ($scope.violationData) {
-                    if ($scope.violationData.NSFFee && $scope.violationData.NSFStatus != "Pending") {
-                        $scope.paymentTotal = $scope.paymentTotal == "0.00" ? $scope.paymentTotal : parseFloat($scope.paymentTotal) + $scope.violationData.NSFFee;
-                        if ($scope.paymentTotal == "0.00") {
-                            $("#popover").popover('hide');
-                            $scope.NSFFeeAdded = false;
-                        }
-                        else {
-                            $("#popover").popover('show');
-                            $scope.NSFFeeAdded = true;
-                        }
+            $scope.getNoticePdf = function (fileName) {
+                var statementData = {
+                    "FileName": fileName,
+                    "DirectoryName": ""
+                };
+                var fileName = "ViolationStatement_" + fileName;
+                var a = document.createElement("a");
+                document.body.appendChild(a);
+                AccountService.getNoticePdf(statementData, function (response) {
+                    if (response) {
+                        var file = new Blob([response.data], { type: 'application/pdf' });
+                        var fileURL = URL.createObjectURL(file);
+                        a.href = fileURL;
+                        a.download = fileName;
+                        a.click();
+                    } else {
+                        response.Message ? FlashService.Error(response.Message) : FlashService.Error("Unable to get notice pdf.");
                     }
-                    var paymentTotal = $scope.paymentTotal;
-                    $scope.paymentTotal = angular.isString($scope.paymentTotal) ? $scope.paymentTotal : paymentTotal.toFixed(2);
+                });
+            };
+
+            $scope.getPaymentUrl = function () {
+                if ($scope.paymentTotal != "0.00" && $scope.paymentMethod) {
+                    $scope.paymentData = {};
+                    $scope.paymentData.AccountGuId = currentUser.AccountGuid;
+                    $scope.paymentData.Amount = parseFloat($scope.paymentTotal);
+                    $scope.paymentData.Paymethod = $scope.paymentMethod;
+                    $scope.paymentData.Language = ($translate.use() == 'en') ? "English" : "Spanish";
+                    $scope.NSFFeeAdded && ($scope.noticeNumbers.indexOf("NSF") == -1) ? $scope.noticeNumbers.push("NSF") : null;
+                    $scope.paymentData.NoticeNumbers = $scope.noticeNumbers;
+
+                    PaymentService.getPaymentURL($scope.paymentData, function (response) {
+                        if (response) {
+                            $scope.payment = {};
+                            $scope.payment.titleId = "FBCTRA Payment Portal";
+                            $scope.payment.contentId = response.paymentUrl;
+                            $scope.payment.data = response;
+                            $scope.payment.showIFrame = true;
+                            $scope.openPaymentModal($scope.payment);
+                        } else {
+                            response.Message ? FlashService.Error(response.Message) : FlashService.Error("Unable to get Payment URL.");
+                        }
+                    });
                 }
-            }
-        });
+            };
 
-        $scope.sort_by = function (newSortingOrder) {
-            if ($scope.sortingOrder == newSortingOrder) {
-                $scope.reverse = !$scope.reverse;
-            }
-            $scope.sortingOrder = newSortingOrder;
-        };
+            $scope.getPaymentStatus = function (paymentData) {
+                PaymentService.getPaymentStatus(paymentData, function (response) {
+                    if (response) {
+                        $scope.payment.data = response;
+                        $scope.getViolationList();
+                    }
+                });
+            };
 
-        $(document).on('click', function (e) {
-            $('[data-toggle="popover"],[data-original-title]').each(function () {
-                if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-                    (($(this).popover('hide').data('bs.popover') || {}).inState || {}).click = false
+            $scope.statusPending = function (status) {
+                return status.toUpperCase() === 'PENDING';
+            };
+
+            $scope.toggleSelection = function toggleSelection($event, item) {
+                if (item.Payable === '1') {
+                    $event.stopPropagation();
+                    var idx = $scope.selection.indexOf(item);
+                    (idx > -1) ? $scope.selection.splice(idx, 1) : $scope.selection.push(item);
+                    var idx2 = $scope.noticeNumbers.indexOf(item.NoticeNumber);
+                    (idx2 > -1) ? $scope.noticeNumbers.splice(idx2, 1) : $scope.noticeNumbers.push(item.NoticeNumber);
+                }
+            };
+
+            $scope.$watchCollection('selection', function (array) {
+                if (array) {
+                    $scope.paymentTotal = array.reduce(function (total, item) {
+                        return total + parseFloat(item.AmountDue);
+                    }, 0).toFixed(2);
+                    if ($scope.violationData) {
+                        if ($scope.violationData.NSFFee && $scope.violationData.NSFStatus != "Pending") {
+                            $scope.paymentTotal = $scope.paymentTotal == "0.00" ? $scope.paymentTotal : parseFloat($scope.paymentTotal) + $scope.violationData.NSFFee;
+                            if ($scope.paymentTotal == "0.00") {
+                                $("#popover").popover('hide');
+                                $scope.NSFFeeAdded = false;
+                            }
+                            else {
+                                $("#popover").popover('show');
+                                $scope.NSFFeeAdded = true;
+                            }
+                        }
+                        var paymentTotal = $scope.paymentTotal;
+                        $scope.paymentTotal = angular.isString($scope.paymentTotal) ? $scope.paymentTotal : paymentTotal.toFixed(2);
+                    }
                 }
             });
-        });
 
-        $scope.getViolationList();
-        $interval(function () { $scope.getViolationList() }, 60000);
-    }
-  ]);
+            $scope.sort_by = function (newSortingOrder) {
+                if ($scope.sortingOrder == newSortingOrder) {
+                    $scope.reverse = !$scope.reverse;
+                }
+                $scope.sortingOrder = newSortingOrder;
+            };
+
+            $(document).on('click', function (e) {
+                $('[data-toggle="popover"],[data-original-title]').each(function () {
+                    if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                        (($(this).popover('hide').data('bs.popover') || {}).inState || {}).click = false
+                    }
+                });
+            });
+
+            $scope.getViolationList();
+
+            $interval(function () {
+                var loggedIn = $rootScope.globals.currentUser;
+                loggedIn ? $scope.getViolationList() : null;
+            }, 60000);
+        }
+    ]);
--- FBCTRA/Services/apiService.js
+++ FBCTRA/Services/apiService.js
@@ -1,129 +1,130 @@
 ﻿angular.module('ApiModule', [])
-.factory('apiService', ['$http', '$rootScope', '$log', '$q', '$resource', 'configurationService', 'localStore', 'localStoreConstants',
-	function ($http, $rootScope, $log, $q, $resource, configurationService, localStore, localStoreConstants) {
-	    'use strict';
+    .factory('apiService', ['$http', '$rootScope', '$state', '$q', '$resource', 'configurationService', 'localStore', 'localStoreConstants',
+        function ($http, $rootScope, $state, $q, $resource, configurationService, localStore, localStoreConstants) {
+            'use strict';
 
-	    var domainApiUrl = configurationService.getVehicleApiUrl();
+            var domainApiUrl = configurationService.getVehicleApiUrl();
 
-	    function getResourcePromise(uri, payload, parameters, resourceConfig, resourceMethod, additionalHeaders, useCacheBuster, useResourceCache) {
-	        // calcualte URL
-	        var url = domainApiUrl + uri;
-	        //$log.debug({ url: url, method: resourceMethod });
+            function getResourcePromise(uri, payload, parameters, resourceConfig, resourceMethod, additionalHeaders, useCacheBuster, useResourceCache) {
+                // calcualte URL
+                var url = domainApiUrl + uri;
+                var headers = angular
+                    .extend({}, {
+                        'Accept': 'application/json,text/html',
+                        "Content-Type": "application/json"
+                    }, additionalHeaders || {});
 
-	        var headers = angular
-	            .extend({}, {
-	                'Accept': 'application/json,text/html',
-	                "Content-Type": "application/json"
-	            }, additionalHeaders || {});
+                // calculate method
+                var method = methodMap[resourceMethod] || methodMap[resourceMethod].get;
+                var isGet = method.verb === 'GET';
+                var isAuthenticating = uri === '/authenticateUser';
 
-	        // calculate method
-	        var method = methodMap[resourceMethod] || methodMap[resourceMethod].get;
-	        var isGet = method.verb === 'GET';
-	        var isAuthenticating = uri === '/authenticateUser';
+                // calculate use of cache buster
+                useCacheBuster = isGet && (typeof (useCacheBuster) === 'boolean' ? useCacheBuster : false);
 
-	        // calculate use of cache buster
-	        useCacheBuster = isGet && (typeof (useCacheBuster) === 'boolean' ? useCacheBuster : false);
-
-	        // initialize resource config
-	        resourceConfig = angular.extend(
-                resourceConfig || {},
-                {
-                    cache: isGet && (!useCacheBuster) && (typeof (useResourceCache) === 'boolean' ? useResourceCache : false)
-                });
-
-	        // initialize resource method configuration
-	        resourceConfig[resourceMethod] = {
-	            headers: headers,
-	            method: method.verb,
-	            //isArray: true
-	        };
-
-	        if (isGet) {
-	            resourceConfig[resourceMethod].isArray = false;
-	        }
-
-	        if (isAuthenticating) {
-	            resourceConfig[resourceMethod].isArray = true;
-	        }
-
-	        if (useCacheBuster) {
-	            // append cache buster
-	            //parameters = parameters || {};
-	            //parameters.cacheBuster = Date.now().toString();
-	        }
-
-	        return method.fn($resource(url, parameters || null, resourceConfig), payload).$promise;
-	    }
-
-	    var methodMap = {
-	        delete: {
-	            verb: 'DELETE',
-	            fn: function (resource) { return resource.delete(); }
-	        },
-	        get: {
-	            verb: 'GET',
-	            fn: function (resource) { return resource.get(); }
-	        },
-	        query: {
-	            verb: 'GET',
-	            fn: function (resource) { return resource.query(); }
-	        },
-	        save: {
-	            verb: 'POST',
-	            fn: function (resource, payload) { return resource.save(payload); }
-	        },
-	        update: {
-	            verb: 'PUT',
-	            fn: function (resource, payload) { return resource.update(payload); }
-	        }
-	    };
-
-	    return {
-	        get: function (url, parameters) {
-	            var loggedIn = $rootScope.globals.currentUser;
-	            if (loggedIn) {
-	                return $http({
-	                    method: 'GET',
-	                    dataType: 'jsonp',
-	                    url: domainApiUrl + url,
-	                    params: parameters
-	                });
-	            }
-	        },
-	        post: function (uri, payload, params) {
-	            return $http.post(domainApiUrl + uri, payload, params
-	            );
-	        },
-	        postFile: function (uri, payload, attachmentToken, filename, params) {
-	            return getResourcePromise(
-	                uri,
-	                payload,
-                    params,
-	                null,
-	                'save',
+                // initialize resource config
+                resourceConfig = angular.extend(
+                    resourceConfig || {},
                     {
-                        'attachmentToken': attachmentToken,
-                        'filename': filename,
-                        'Content-Type': undefined,
+                        cache: isGet && (!useCacheBuster) && (typeof (useResourceCache) === 'boolean' ? useResourceCache : false)
+                    });
+
+                // initialize resource method configuration
+                resourceConfig[resourceMethod] = {
+                    headers: headers,
+                    method: method.verb,
+                    //isArray: true
+                };
+
+                if (isGet) {
+                    resourceConfig[resourceMethod].isArray = false;
+                }
+
+                if (isAuthenticating) {
+                    resourceConfig[resourceMethod].isArray = true;
+                }
+
+                if (useCacheBuster) {
+                    // append cache buster
+                    //parameters = parameters || {};
+                    //parameters.cacheBuster = Date.now().toString();
+                }
+
+                return method.fn($resource(url, parameters || null, resourceConfig), payload).$promise;
+            }
+
+            var methodMap = {
+                delete: {
+                    verb: 'DELETE',
+                    fn: function (resource) { return resource.delete(); }
+                },
+                get: {
+                    verb: 'GET',
+                    fn: function (resource) { return resource.get(); }
+                },
+                query: {
+                    verb: 'GET',
+                    fn: function (resource) { return resource.query(); }
+                },
+                save: {
+                    verb: 'POST',
+                    fn: function (resource, payload) { return resource.save(payload); }
+                },
+                update: {
+                    verb: 'PUT',
+                    fn: function (resource, payload) { return resource.update(payload); }
+                }
+            };
+
+            return {
+                get: function (url, parameters) {
+                    var loggedIn = $rootScope.globals.currentUser;
+                    if (loggedIn) {
+                        return $http({
+                            method: 'GET',
+                            dataType: 'jsonp',
+                            url: domainApiUrl + url,
+                            params: parameters
+                        });
+                    } else {
+                        $state.go('login', {}, { reload: true });
+                        return $q.reject(null);
                     }
-                );
-	        },
-	        put: function (uri, payload, params) {
-	            return getResourcePromise(
-                    uri,
-                    payload,
-                    params,
-                    null,
-                    'update');
-	        },
-	        remove: function (uri, params) {
-	            return getResourcePromise(
-                    uri,
-                    null,
-                    params,
-                    null,
-                    'delete');
-	        }
-	    };
-	}
-]);
+                },
+                post: function (uri, payload, params) {
+                    return $http.post(domainApiUrl + uri, payload, params
+                    );
+                },
+                postFile: function (uri, payload, attachmentToken, filename, params) {
+                    return getResourcePromise(
+                        uri,
+                        payload,
+                        params,
+                        null,
+                        'save',
+                        {
+                            'attachmentToken': attachmentToken,
+                            'filename': filename,
+                            'Content-Type': undefined,
+                        }
+                    );
+                },
+                put: function (uri, payload, params) {
+                    return getResourcePromise(
+                        uri,
+                        payload,
+                        params,
+                        null,
+                        'update');
+                },
+                remove: function (uri, params) {
+                    return getResourcePromise(
+                        uri,
+                        null,
+                        params,
+                        null,
+                        'delete');
+                }
+            };
+        }
+    ]);
